7fad5c63d46888a0c08c155faa90159ae5db47bb
Jorge Sanchez <jorgeluissanchezbarreneche@gmail.com>
2023-12-16 20:31:26 -0500

    kool

--- components/recent-changes.tsx
+++ components/recent-changes.tsx
@@ -12,7 +12,7 @@ import { ScrollArea } from "@/components/ui/scroll-area"
         <ScrollArea className="h-56 px-4">
       <div className="space-y-8">
         {[1,2,3,4,5,6,7,8,9,10].map((item) => (
-        <div className="flex items-center">
+        <div className="flex items-center" key={item}>
           <Avatar className="h-9 w-9">
             <AvatarImage src="/avatars/01.png" alt="Avatar" />
             <AvatarFallback>OM</AvatarFallback>
